a9ba57fee49a18a46e2a7cae7c9c0086e2839f01
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2023-08-08 16:15:42 +0200

    License header

--- src/main/java/org/fl/avoidLock/WorkerInformation.java
+++ src/main/java/org/fl/avoidLock/WorkerInformation.java
@@ -1,16 +1,40 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2023 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.fl.avoidLock;
 
 public class WorkerInformation {
 
-	private String status ;
-	private int step ;
-	private long remainingTime ;
-	
+	private String status;
+	private int step;
+	private long remainingTime;
+
 	public WorkerInformation() {
 		super();
-		step = 0 ;
-		status = "Unknown" ;
-		remainingTime = Control.getMaxDuration() ;
+		step = 0;
+		status = "Unknown";
+		remainingTime = Control.getMaxDuration();
 	}
 
 	public String getStatus() {
@@ -28,7 +52,6 @@ public class WorkerInformation {
 	public void setStep(int step) {
 		this.step = step;
 	}
-
 
 	public long getRemainingTime() {
 		return remainingTime;
