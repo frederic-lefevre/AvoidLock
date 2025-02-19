e06dbd94d458110518167af787fa60981624778c
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-02-19 18:00:41 +0100

    Reformat code

--- src/main/java/org/fl/avoidLock/AvoidLock.java
+++ src/main/java/org/fl/avoidLock/AvoidLock.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2024 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -39,136 +39,136 @@ public class AvoidLock  extends SwingWorker<String,WorkerInformation> {
 
 	private static final Logger avoidLockLog = Logger.getLogger(AvoidLock.class.getName());
 	
-	private final UiControl   startStop ;
+	private final UiControl startStop;
 	private final ProcessInfo stepsInfo;
-	
-	private int 			  step ;
-	private WorkerInformation wkInfos ;
-	private Robot 			  mouseRobot ;
+
+	private int step;
+	private WorkerInformation wkInfos;
+	private Robot mouseRobot;
 
 	public AvoidLock(UiControl uc, ProcessInfo pi) {
 		super();
-		startStop = uc ;
-		stepsInfo = pi ;
-		step 	  = 0 ;
+		startStop = uc;
+		stepsInfo = pi;
+		step = 0;
 		try {
 			mouseRobot = new Robot();
 		} catch (AWTException e) {
-			avoidLockLog.log(Level.SEVERE, "AWT exception when creating robot", e) ;
-			mouseRobot = null ;
+			avoidLockLog.log(Level.SEVERE, "AWT exception when creating robot", e);
+			mouseRobot = null;
 		}
-		wkInfos = new WorkerInformation() ;
+		wkInfos = new WorkerInformation();
 	}
 	
 
-	 @Override 
-	 public String doInBackground() {
-	
-		step = 0 ;
-		Chronometre chronos = new Chronometre() ;
-		
-		while  (Control.getRemainingTime() > 0) {
-				
-			avoidLockLog.fine("Process step " + step + "; Remaining time=" + Control.getRemainingTime()) ;
-			
-			if (! startStop.isRunning()) {
-				chronos = new Chronometre() ;
-				Control.setRemainingTime(Control.getMaxDuration()*60000) ;
-				step = 0 ;
-				wkInfos.setStep(step) ;
-				wkInfos.setStatus("Reset done.") ;
+	@Override
+	public String doInBackground() {
+
+		step = 0;
+		Chronometre chronos = new Chronometre();
+
+		while (Control.getRemainingTime() > 0) {
+
+			avoidLockLog.fine(() -> "Process step " + step + "; Remaining time=" + Control.getRemainingTime());
+
+			if (!startStop.isRunning()) {
+				chronos = new Chronometre();
+				Control.setRemainingTime(Control.getMaxDuration() * 60000);
+				step = 0;
+				wkInfos.setStep(step);
+				wkInfos.setStatus("Reset done.");
 				wkInfos.setRemainingTime(Control.getRemainingTime());
-				
-				publish(wkInfos) ;
-				avoidLockLog.fine("Reset to initial state") ;
-				
-				while (! startStop.isRunning()) {
-					// the worker is not running	
-					// sleep some time, 
-					
+
+				publish(wkInfos);
+				avoidLockLog.fine("Reset to initial state");
+
+				while (!startStop.isRunning()) {
+					// the worker is not running
+					// sleep some time,
+
 					try {
-						Thread.sleep(10) ;
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 					}
 				}
 				chronos.start();
-				avoidLockLog.fine("Process is restarted") ;
+				avoidLockLog.fine("Process is restarted");
 			}
 			if ((startStop.isPaused() && startStop.isRunning())) {
 				long v = chronos.pause();
-				wkInfos.setStep(step) ;
-				wkInfos.setStatus("Paused. ") ;
+				wkInfos.setStep(step);
+				wkInfos.setStatus("Paused. ");
 				Control.setRemainingTime(Control.getRemainingTime() - v);
 				wkInfos.setRemainingTime(Control.getRemainingTime());
-				publish(wkInfos) ;
-				avoidLockLog.finest("Process is paused") ;
-				
-				while (startStop.isPaused() && startStop.isRunning()) {				
-					
+				publish(wkInfos);
+				avoidLockLog.finest("Process is paused");
+
+				while (startStop.isPaused() && startStop.isRunning()) {
+
 					// the simulator is paused
 					// sleep some time
 					try {
-						Thread.sleep(10) ;
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 					}
 				}
 				chronos.start();
 				avoidLockLog.finest("Process is resumed");
-				
+
 			}
-			
+
 			Control.setRemainingTime(Control.getRemainingTime() - chronos.getDeltaValue());
-			wkInfos.setStep(step) ;
-			wkInfos.setStatus("Running. ") ;
-			wkInfos.setRemainingTime(Control.getRemainingTime());			
-			publish(wkInfos) ;			
-			
+			wkInfos.setStep(step);
+			wkInfos.setStatus("Running. ");
+			wkInfos.setRemainingTime(Control.getRemainingTime());
+			publish(wkInfos);
+
 			// Move the mouse to avoid lock
-			int x = MouseInfo.getPointerInfo().getLocation().x ;
-			int y = MouseInfo.getPointerInfo().getLocation().y ;
-			int pas ;
+			int x = MouseInfo.getPointerInfo().getLocation().x;
+			int y = MouseInfo.getPointerInfo().getLocation().y;
+			int pas;
 			if (step % 2 == 0) {
-				pas = Control.getNbPixels() ;
+				pas = Control.getNbPixels();
 			} else {
-				pas = 0 - Control.getNbPixels() ;
+				pas = 0 - Control.getNbPixels();
 			}
-			mouseRobot.mouseMove(x+ pas, y);
-			
+			mouseRobot.mouseMove(x + pas, y);
+
 			// sleep some time
 			try {
-				Thread.sleep(Control.getTiming()) ;
+				Thread.sleep(Control.getTiming());
 			} catch (InterruptedException e) {
 			}
-			
+
 			// go to next step
-			step = step + 1 ;
+			step = step + 1;
 		}
-		return "Avoid lock ended" ;
-	 }
+		return "Avoid lock ended";
+	}
 	 
 	 @Override
-     protected void process(java.util.List<WorkerInformation> chunks) {
-        // Get the latest result from the list
+	 protected void process(java.util.List<WorkerInformation> chunks) {
+		 // Get the latest result from the list
 		 WorkerInformation latestResult = chunks.get(chunks.size() - 1);
-		 
-        stepsInfo.setStepNumber(latestResult.getStep());
-        stepsInfo.setSimulStatus(latestResult.getStatus());
-        stepsInfo.setRemaingTime(Control.getRemainingTimeString());
-        startStop.setRemainingTime(latestResult.getRemainingTime());
-     }
+
+		 stepsInfo.setStepNumber(latestResult.getStep());
+		 stepsInfo.setSimulStatus(latestResult.getStatus());
+		 stepsInfo.setRemaingTime(Control.getRemainingTimeString());
+		 startStop.setRemainingTime(latestResult.getRemainingTime());
+	 }
 
 	 @Override
-     protected void done() {
-		 
+	 protected void done() {
+
 		 try {
-			 stepsInfo.setSimulStatus(get()) ;
-			 startStop.deactivate() ;
-		} catch (InterruptedException e) {
-			avoidLockLog.severe("Exception when getting process result: " + e) ;
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			avoidLockLog.severe("Exception when getting process result: " + e) ;
-			e.printStackTrace();
-		}
+			 stepsInfo.setSimulStatus(get());
+			 startStop.deactivate();
+		 } catch (InterruptedException e) {
+			 avoidLockLog.severe("Exception when getting process result: " + e);
+			 e.printStackTrace();
+		 } catch (ExecutionException e) {
+			 avoidLockLog.severe("Exception when getting process result: " + e);
+			 e.printStackTrace();
+		 }
 	 }
 }
