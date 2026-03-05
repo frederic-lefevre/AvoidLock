bf89c294fa48b57d849f7f24dc2ea71b964916f8
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2026-03-05 15:02:12 +0100

    Java 25, Jackson 3

--- src/test/java/org/fl/avoidLock/ControlTest.java
+++ src/test/java/org/fl/avoidLock/ControlTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2025 Frederic Lefevre
+Copyright (c) 2017, 2026 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -31,8 +31,8 @@ import java.net.URISyntaxException;
 import org.fl.util.RunningContext;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.JsonNode;
 
 class ControlTest {
 
@@ -54,17 +54,17 @@ class ControlTest {
 		.satisfiesExactlyInAnyOrder(
 				buildInfo -> { 
 					assertThat(buildInfo.get("moduleName")).isNotNull();
-					assertThat(buildInfo.get("moduleName").asText()).isEqualTo(APPLICATION_NAME);
+					assertThat(buildInfo.get("moduleName").asString()).isEqualTo(APPLICATION_NAME);
 				},
 				buildInfo -> { 
 					assertThat(buildInfo.get("moduleName")).isNotNull();
-					assertThat(buildInfo.get("moduleName").asText()).isEqualTo("org.fl.util");
+					assertThat(buildInfo.get("moduleName").asString()).isEqualTo("org.fl.util");
 				}
 				);
 	}
 	
 	@Test
-	void buildInformationTest() throws JsonProcessingException, URISyntaxException {
+	void buildInformationTest() throws JacksonException, URISyntaxException {
 		
 		RunningContext runningContext = Control.getRunningContext();
 		
@@ -83,7 +83,7 @@ class ControlTest {
 	private void assertModuleBuildInfo(JsonNode buildInfo, String moduleName) {
 		assertThat(buildInfo).hasSize(11);
 		assertThat(buildInfo.get("moduleName")).isNotNull();
-		assertThat(buildInfo.get("moduleName").asText()).isEqualTo(moduleName);
+		assertThat(buildInfo.get("moduleName").asString()).isEqualTo(moduleName);
 		assertThat(buildInfo.has("version")).isTrue();
 		assertThat(buildInfo.has("buildtime")).isTrue();
 		assertThat(buildInfo.has("builder")).isTrue();
